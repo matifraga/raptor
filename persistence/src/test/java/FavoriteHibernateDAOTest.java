ce05022b0e65bb9a658abedb2d21c2ba847edfc9
lmarzora <lmarzora@itba.edu.ar>
2017-02-07 01:19:19 -0300

    tests

--- persistence/src/test/java/FavoriteHibernateDAOTest.java
+++ persistence/src/test/java/FavoriteHibernateDAOTest.java
@@ -1,7 +1,71 @@
-package PACKAGE_NAME;
+import ar.edu.itba.paw.models.Tweet;
+import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.persistence.FavoriteHibernateDAO;
+import ar.edu.itba.paw.persistence.TweetDAO;
+import ar.edu.itba.paw.persistence.UserDAO;
+import ar.edu.itba.paw.services.TweetService;
+import org.junit.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
 
 /**
  * Created by luis on 2/6/17.
  */
+
 public class FavoriteHibernateDAOTest {
+
+    @Autowired
+    private TweetDAO tweetDAO;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    private FavoriteHibernateDAO favoriteHibernateDAO;
+    private static Tweet tweet;
+    private static User user;
+
+    private static String USERNAME = "username", FIRST_NAME = "firstName",
+            LAST_NAME = "lastName", EMAIL = "email@raptor.com", PASSWORD = "rawr";
+    private static boolean VERIFIED = true;
+
+    private static String MSG = "I hate testing";
+    private static Date date;
+    private static int INITIAL_LIKES = 0, RETWEETS = 0;
+
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        date = new Date(System.currentTimeMillis());
+        user = new User(USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PASSWORD,VERIFIED);
+        tweet = new Tweet(MSG,user,date,RETWEETS,INITIAL_LIKES,null);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    @Transactional
+    @Rollback(true)
+    void favoriteTest() {
+        user =  userDAO.create(USERNAME,PASSWORD,EMAIL,FIRST_NAME,LAST_NAME);
+        tweet = tweetDAO.create(MSG,user);
+        favoriteHibernateDAO.favorite(tweet,user);
+        assert (favoriteHibernateDAO.isFavorited(tweet,user));
+    }
+
+
 }
--- persistence/src/test/java/NotificationHibernateDAOTest.java
+++ persistence/src/test/java/NotificationHibernateDAOTest.java
@@ -1,7 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  * Created by luis on 2/6/17.
  */
 public class NotificationHibernateDAOTest {
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
