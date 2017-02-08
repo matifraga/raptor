77ed8bf0c9e419272c65e7ad94198a52ae40cb37
lmarzora <lmarzora@itba.edu.ar>
2017-02-08 17:52:09 -0300

    new front + removed default servlet

--- webapp/src/main/webapp/scripts/directives/rawr-list.9d6dac2a.js
+++ webapp/src/main/webapp/scripts/directives/rawr-list.9d6dac2a.js
@@ -1,1 +1,1 @@
-define(["AnguRaptor","controllers/RawrListCtrl"],function(a){a.directive("rawrList",function(){return{restrict:"E",templateUrl:"./views/rawr-list.fed66e9e.html",controller:"RawrListCtrl",scope:{items:"="}}})});
+define(["AnguRaptor","controllers/RawrListCtrl"],function(a){a.directive("rawrList",function(){return{restrict:"E",templateUrl:"./views/rawr-list.24809e61.html",controller:"RawrListCtrl",scope:{items:"="}}})});
